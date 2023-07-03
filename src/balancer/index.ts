52099aad47a193b8c02753356fbc5f559909618e
Yu Mentsel <yu.mentsel@gmail.com>
2023-07-04 02:56:38 +0300

    fix: fix balancer

--- src/balancer/index.ts
+++ src/balancer/index.ts
@@ -1,17 +1,13 @@
-import { IncomingMessage, Server, ServerResponse } from 'node:http';
+import { Server } from 'node:http';
 import cluster from 'node:cluster';
 import { cpus } from 'node:os';
 import { User } from '../types/interfaces';
-
-export type DataBase = User[];
-
-export let DATA_BASE: DataBase = [];
 
 export const balancer = (PORT: number, server: Server, users: User[]) => {
   if (cluster.isPrimary) {
     console.log(`Primary ${process.pid} is running`);
     cpus().forEach((_, i) => {
-      cluster.fork({ workerPort: PORT + i + 1 });
+      cluster.fork({ workerPort: PORT + i });
     });
     cluster.on('exit', () => {
       cluster.fork();
